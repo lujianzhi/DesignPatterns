6717cb7e320719ced9609302f4d7a4d9f09a5f15
jianzhi.lu <jianzhi.lu@beibei.com>
2019-08-21 21:57:11 +0800

    观察者模式分享demo

--- src/com/ian/observer/example2/wrong/boy/ComputerBoy.java
+++ src/com/ian/observer/example2/wrong/boy/ComputerBoy.java
@@ -11,7 +11,7 @@ public class ComputerBoy implements Boy {
 
     @Override
     public void careWay() {
-        System.out.println("计算机系boy:主动去获取女神动态效率太低了，" +
-                "还是写个观察者模式让女神主动通知我吧，她一定会感动的");
+        System.out.println("自己去获取女神动态不及时啊，"
+                           + "还是写个观察者模式让女神主动通知我吧，她一定会感动的");
     }
 }
